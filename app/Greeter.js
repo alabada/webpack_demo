16cd92454013fe3972214192c658e24bc94064a8
zhida.wen <zhida_wen@163.com>
2017-07-18 14:12:05 +0800

    add some

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,5 +1,17 @@
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = "Hi there and greetings!";
-  return greet;
-};
+import React, {Component} from 'react'
+import config from './config.json';
+import styles from './Greeter.css';//导入
+
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div className={styles.root}>//添加类名
+        {config.greetText}
+      </div>
+    );
+  }
+}
+
+
+export default Greeter
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,35 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+  devtool: 'eval-source-map',
+
+  entry:  __dirname + "/app/main.js",
   output: {
-    path: __dirname + "/public",//打包后的文件存放的地方
-    filename: "bundle.js"//打包后输出文件的文件名
+    path: __dirname + "/build",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: "json" },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.css$/, loader: 'style!css?modules!postcss' }
+    ]
+  },
+  postcss: [
+    require('autoprefixer')
+  ],
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+    })
+  ],
+
+  devServer: {
+    colors: true,
+    historyApiFallback: true,
+    inline: true
   }
 }
